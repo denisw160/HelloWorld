fe293a1f2efd507395fd8ba21167e379bec4fd19
Denis Wirries <denis@wirries.me>
2016-10-20 19:26:26 +0200

    Adding Test

---
+++ src/test/java/de/netpage/helloworld/HelloWorldApplicationTest.java
@@ -0,0 +1,19 @@
+package de.netpage.helloworld;
+
+import org.junit.Test;
+
+/**
+ * This is an Test for application.
+ *
+ * @author Denis Wirries
+ * @version 1.0
+ * @since 20.10.16
+ */
+public class HelloWorldApplicationTest {
+
+    @Test
+    public void main() throws Exception {
+        HelloWorldApplication.main(null);
+    }
+
+}
