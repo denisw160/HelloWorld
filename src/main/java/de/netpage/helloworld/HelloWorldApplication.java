954a7d4d851777f2d3dacc43ea50d31588a5c123
Denis Wirries <denis@wirries.me>
2016-10-20 19:20:40 +0200

    Adding Main Class

---
+++ src/main/java/de/netpage/helloworld/HelloWorldApplication.java
@@ -0,0 +1,21 @@
+package de.netpage.helloworld;
+
+/**
+ * This is the Main Class for the Application.
+ *
+ * @author Denis Wirries
+ * @version 1.0
+ * @since 20.10.16
+ */
+public class HelloWorldApplication {
+
+    /**
+     * Start the application.
+     *
+     * @param args Arguments from the program start
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
---
+++ src/main/java/de/netpage/helloworld/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This is the main package of the application.
+ *
+ * @author Denis Wirries
+ * @version 1.0
+ * @since 20.10.16
+ */
+package de.netpage.helloworld;
