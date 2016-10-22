100abcce344950a813684831651e255528dfe82e
Denis Wirries <denis@wirries.me>
2016-10-22 10:35:03 +0200

    Adding Findings

---
+++ src/main/java/de/netpage/helloworld/DeadCode.java
@@ -0,0 +1,16 @@
+package de.netpage.helloworld;
+
+/**
+ * This is a dead code class.
+ *
+ * @author Denis Wirries
+ * @version 1.0
+ * @since 22.10.16
+ */
+public class DeadCode {
+
+    public void doNothing() {
+        // This methode don't do anything.
+    }
+
+}
